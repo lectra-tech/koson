d298d93d3d1f44c58ab9277cfd205105c73900f4
Nicolas Comet <nicolas.comet@gmail.com>
2018-12-11 13:34:00 +0100

    Custom values are rendered (#2)
    * allow custom Any types
    
    * refactor functions

--- src/main/kotlin/io/github/ncomet/koson/Koson.kt
+++ src/main/kotlin/io/github/ncomet/koson/Koson.kt
@@ -14,6 +14,10 @@ private data class BooleanType(val value: Boolean) : KosonType() {
     override fun toString(): String = value.toString()
 }
 
+private data class CustomType(val value: Any) : KosonType() {
+    override fun toString(): String = "\"${value.toString().replace("\"", "\\\"")}\""
+}
+
 private object NullType : KosonType() {
     override fun toString(): String = "null"
 }
@@ -32,6 +36,19 @@ val arrayØ: ArrayType = ArrayType(emptyList())
 object array {
     operator fun get(vararg elements: Any?): ArrayType =
         ArrayType(elements.map { toAllowedType(it) }.toList())
+
+    private fun toAllowedType(value: Any?): KosonType {
+        return when (value) {
+            is String -> StringType(value)
+            is Number -> NumberType(value)
+            is Boolean -> BooleanType(value)
+            is ObjectType -> value
+            is ArrayType -> value
+            null -> NullType
+            array -> throw KosonException("<array> keyword cannot be used as value, to describe an empty array, use <arrayØ> instead")
+            else -> CustomType(value)
+        }
+    }
 }
 
 fun obj(block: Koson.() -> Unit): ObjectType {
@@ -42,42 +59,33 @@ fun obj(block: Koson.() -> Unit): ObjectType {
 
 class Koson(internal val objectType: ObjectType = ObjectType()) {
 
-    infix fun String.to(value: String) {
-        addValueIfFreeKey(StringType(value))
-    }
+    infix fun String.to(value: String) = addValueIfKeyIsAvailable(StringType(value))
 
-    infix fun String.to(value: Number) {
-        addValueIfFreeKey(NumberType(value))
-    }
+    infix fun String.to(value: Number) = addValueIfKeyIsAvailable(NumberType(value))
 
-    infix fun String.to(value: Boolean) {
-        addValueIfFreeKey(BooleanType(value))
-    }
+    infix fun String.to(value: Boolean) = addValueIfKeyIsAvailable(BooleanType(value))
 
     @Suppress("UNUSED_PARAMETER")
-    infix fun String.to(value: Nothing?) {
-        addValueIfFreeKey(NullType)
-    }
+    infix fun String.to(value: Nothing?) = addValueIfKeyIsAvailable(NullType)
 
-    infix fun String.to(value: ObjectType) {
-        addValueIfFreeKey(value)
-    }
+    infix fun String.to(value: Any) = addValueIfKeyIsAvailable(CustomType(value))
 
-    infix fun String.to(value: ArrayType) {
-        addValueIfFreeKey(value)
-    }
+    infix fun String.to(value: ObjectType) = addValueIfKeyIsAvailable(value)
 
-    infix fun String.to(value: Any): Nothing =
-            throw KosonException("value <$value> of type [${value.javaClass.simpleName}] is not one of allowed JSON value types (String, Number, Boolean, null, obj{}, array[...] or arrayØ)")
+    infix fun String.to(value: ArrayType) = addValueIfKeyIsAvailable(value)
 
     @Suppress("UNUSED_PARAMETER")
     infix fun String.to(value: array): Nothing =
-            throw KosonException("<array> keyword cannot be used as value, to describe an empty array, use <arrayØ> instead")
+        throw KosonException("<array> keyword cannot be used as value, to describe an empty array, use <arrayØ> instead")
+
+    @Suppress("UNUSED_PARAMETER")
+    infix fun String.to(value: Koson): Nothing =
+        throw KosonException("<this> keyword cannot be used as value inside an obj { }")
 
     infix fun Any.to(value: Any?): Nothing =
-            throw KosonException("key <$this> of ($this to $value) must be of type String")
+        throw KosonException("key <$this> of ($this to $value) must be of type String")
 
-    private fun String.addValueIfFreeKey(type: KosonType) {
+    private fun String.addValueIfKeyIsAvailable(type: KosonType) {
         if (!objectType.values.containsKey(this)) {
             objectType.values[this] = type
         } else {
@@ -88,16 +96,3 @@ class Koson(internal val objectType: ObjectType = ObjectType()) {
 }
 
 class KosonException(message: String) : RuntimeException(message)
-
-private fun toAllowedType(value: Any?): KosonType {
-    return when (value) {
-        is String -> StringType(value)
-        is Number -> NumberType(value)
-        is Boolean -> BooleanType(value)
-        is ObjectType -> value
-        is ArrayType -> value
-        null -> NullType
-        array -> throw KosonException("<array> keyword cannot be used as value, to describe an empty array, use <arrayØ> instead")
-        else -> throw KosonException("value <$value> of type [${value.javaClass.simpleName}] is not one of allowed JSON value types (String, Number, Boolean, null, obj{}, array[...] or arrayØ)")
-    }
-}
