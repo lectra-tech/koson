300dfe8ae886dcaf4681ce22d99ed109e3601d4a
Nicolas Comet <nicolas.comet@gmail.com>
2018-12-11 21:53:44 +0100

    improving test

--- src/main/kotlin/io/github/ncomet/koson/Koson.kt
+++ src/main/kotlin/io/github/ncomet/koson/Koson.kt
@@ -35,15 +35,16 @@ object array : ArrayType() {
     operator fun get(vararg elements: Any?): ArrayType =
         ArrayType(elements.map { toAllowedType(it) }.toList())
 
-    private fun toAllowedType(value: Any?): KosonType = when (value) {
-        is String -> StringType(value)
-        is Number -> NumberType(value)
-        is Boolean -> BooleanType(value)
-        is ObjectType -> value
-        is ArrayType -> value
-        null -> NullType
-        else -> CustomType(value)
-    }
+    private fun toAllowedType(value: Any?): KosonType =
+        when (value) {
+            is String -> StringType(value)
+            is Number -> NumberType(value)
+            is Boolean -> BooleanType(value)
+            is ObjectType -> value
+            is ArrayType -> value
+            null -> NullType
+            else -> CustomType(value)
+        }
 }
 
 fun obj(block: Koson.() -> Unit): ObjectType {
--- src/test/kotlin/io/github/ncomet/koson/KosonTest.kt
+++ src/test/kotlin/io/github/ncomet/koson/KosonTest.kt
@@ -13,17 +13,6 @@ class KosonTest : WithAssertions {
     @Test
     fun `empty object`() {
         assertThat(obj { }.toString()).isEqualTo("{}")
-        obj {
-            "string" to "value"
-            "int" to 9
-            "double" to 7.6
-            "float" to 3.2f
-            "boolean" to false
-            "object" to obj { }
-            "emptyArray" to array
-            "array" to array["test"]
-            "null" to null
-        }
     }
 
     @Test
@@ -36,6 +25,10 @@ class KosonTest : WithAssertions {
         array[this]
     }
 
+    object SimpleObject {
+        override fun toString(): String = this.javaClass.simpleName
+    }
+
     object ContainsDoubleQuotes {
         override fun toString(): String = "\"unfor\"tunate\""
     }
@@ -44,22 +37,44 @@ class KosonTest : WithAssertions {
     fun `object with all possible types of value`() {
         assertThat(obj {
             "string" to "value"
-            "int" to 9
             "double" to 7.6
             "float" to 3.2f
+            "long" to 34L
+            "int" to 9
+            "char" to 'e'
+            "short" to 12.toShort()
+            "byte" to 0x32
             "boolean" to false
             "object" to obj { }
             "emptyArray" to array
             "array" to array["test"]
             "null" to null
+            "any" to SimpleObject
             "custom" to ContainsDoubleQuotes
-        }.toString()).isEqualTo("{\"string\":\"value\",\"int\":9,\"double\":7.6,\"float\":3.2,\"boolean\":false,\"object\":{},\"emptyArray\":[],\"array\":[\"test\"],\"null\":null,\"custom\":\"\\\"unfor\\\"tunate\\\"\"}")
+        }.toString()).isEqualTo("{\"string\":\"value\",\"double\":7.6,\"float\":3.2,\"long\":34,\"int\":9,\"char\":\"e\",\"short\":12,\"byte\":50,\"boolean\":false,\"object\":{},\"emptyArray\":[],\"array\":[\"test\"],\"null\":null,\"any\":\"SimpleObject\",\"custom\":\"\\\"unfor\\\"tunate\\\"\"}")
     }
 
     @Test
     internal fun `array with all possible types of value`() {
-        assertThat(array["value", 9, 7.6, 3.2f, false, obj { }, array, array["test"], null, ContainsDoubleQuotes].toString())
-            .isEqualTo("[\"value\",9,7.6,3.2,false,{},[],[\"test\"],null,\"\\\"unfor\\\"tunate\\\"\"]")
+        assertThat(
+            array[
+                    "value",
+                    7.6,
+                    3.2f,
+                    34L,
+                    9,
+                    'e',
+                    12.toShort(),
+                    0x32,
+                    false,
+                    obj { },
+                    array,
+                    array["test"],
+                    null,
+                    SimpleObject,
+                    ContainsDoubleQuotes
+            ].toString()
+        ).isEqualTo("[\"value\",7.6,3.2,34,9,\"e\",12,50,false,{},[],[\"test\"],null,\"SimpleObject\",\"\\\"unfor\\\"tunate\\\"\"]")
     }
 
     @Nested
@@ -86,7 +101,7 @@ class KosonTest : WithAssertions {
 
         @Test
         @Suppress("UNUSED_EXPRESSION")
-        fun `expression object not containing a Koson_to() should do nothing`() {
+        fun `object not containing a to() should do nothing`() {
             assertThat(obj { "content" }.toString()).isEqualTo("{}")
         }
     }
@@ -94,39 +109,50 @@ class KosonTest : WithAssertions {
     @Nested
     inner class MoreComplexCases : WithAssertions {
         @Test
-        fun `testing all types in an object`() {
+        fun `constructing a bit more complex object`() {
             val obj = obj {
                 "key" to 3.4
                 "anotherKey" to array["test", "test2", 1, 2.433, true]
                 "nullsAreAllowedToo" to null
+                "array" to array[
+                        obj {
+                            "double" to 33.4
+                            "float" to 345f
+                            "long" to 21L
+                            "int" to 42
+                            "char" to 'a'
+                            "byte" to 0xAA
+                            "otherArray" to array
+                            "simpleObject" to SimpleObject
+                        }
+                ]
             }
 
             assertThat("$obj")
-                .isEqualTo("{\"key\":3.4,\"anotherKey\":[\"test\",\"test2\",1,2.433,true],\"nullsAreAllowedToo\":null}")
+                .isEqualTo("{\"key\":3.4,\"anotherKey\":[\"test\",\"test2\",1,2.433,true],\"nullsAreAllowedToo\":null,\"array\":[{\"double\":33.4,\"float\":345.0,\"long\":21,\"int\":42,\"char\":\"a\",\"byte\":170,\"otherArray\":[],\"simpleObject\":\"SimpleObject\"}]}")
         }
 
         @Test
-        fun `testing all types in an object inlined`() {
-            val obj = obj {
-                "key" to 3.4; "anotherKey" to array["test", "test2", 1, 2.433, true]; "nullsAreAllowedToo" to null
-            }
-
-            assertThat("$obj")
-                .isEqualTo("{\"key\":3.4,\"anotherKey\":[\"test\",\"test2\",1,2.433,true],\"nullsAreAllowedToo\":null}")
-        }
-
-        @Test
-        fun `testing all types in an array`() {
-            val array = array[
-                    "koson",
+        fun `contructing a bit more complex array`() {
+            val array = array["koson", 33.4, 345f, 21L, 42, 'a', 0x21,
                     obj {
-                        "key" to 3.4
-                        "anotherKey" to array["test", "test2", 1, 2.333, true, null]
+                        "aKey" to "value"
+                        "insideArray" to array
+                        "otherArray" to array["element", ContainsDoubleQuotes, obj { }]
                     }
             ]
 
             assertThat("$array")
-                .isEqualTo("[\"koson\",{\"key\":3.4,\"anotherKey\":[\"test\",\"test2\",1,2.333,true,null]}]")
+                .isEqualTo("[\"koson\",33.4,345.0,21,42,\"a\",33,{\"aKey\":\"value\",\"insideArray\":[],\"otherArray\":[\"element\",\"\\\"unfor\\\"tunate\\\"\",{}]}]")
+        }
+
+        @Test
+        fun `testing an object inlined`() {
+            val obj =
+                obj { "key" to 3.4; "anotherKey" to array["test", "test2", 1, 2.433, true]; "nullsAreAllowedToo" to null }
+
+            assertThat("$obj")
+                .isEqualTo("{\"key\":3.4,\"anotherKey\":[\"test\",\"test2\",1,2.433,true],\"nullsAreAllowedToo\":null}")
         }
     }
 
@@ -134,13 +160,15 @@ class KosonTest : WithAssertions {
     inner class ExceptionCases : WithAssertions {
 
         @Test
-        fun `object must throw KosonException when duplicate key`() {
-            assertThrows<KosonException> {
+        fun `object construction must throw KosonException when duplicate key`() {
+            val message = assertThrows<KosonException> {
                 obj {
                     "key" to "myVal"
                     "key" to 1.65
                 }
-            }
+            }.message
+
+            assertThat(message).isEqualTo("key <key> of (key to 1.65) is already defined for json object")
         }
 
     }
